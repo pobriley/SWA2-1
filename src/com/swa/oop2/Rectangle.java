a5d804a7cd776828f813ea9254e0be6f73bc8fe3
pobriley <624259030@webmail.npru.ac.th>
2021-07-12 15:33:42 +0700

    Initial commit

---
+++ src/com/swa/oop2/Rectangle.java
@@ -0,0 +1,10 @@
+package com.swa.oop2;
+
+public class Rectangle extends Shape {
+
+	
+	public double getArea() {
+		return getHeight() * getWidth();
+	}
+
+}
---
+++ src/com/swa/oop2/Triangle.java
@@ -0,0 +1,9 @@
+package com.swa.oop2;
+
+public class Triangle extends Shape {
+
+	public double getArea() {
+		return (getHeight() * getWidth()) / 2;
+	}
+
+}
